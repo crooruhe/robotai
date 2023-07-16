b92653788370b2fd3b9ac68d2e64caaa51857d47
unknown <cruhe@yahoo.com>
2023-07-15 23:20:11 -0500

    updated from doc examples

--- picameratest.py
+++ picameratest.py
@@ -1,20 +1,18 @@
-# i think a lot of this is old picamera 1 so it needs to be updated to 2 which is what the picamera 3 uses
+""" # i think a lot of this is old picamera 1 so it needs to be updated to 2 which is what the picamera 3 uses
 
 import time
-import picamera
-import picamera.array
+from picamera2 import Picamera2
 import cv2
 
-camera = picamera.PiCamera()
-
-camera.resolution = (640, 480)
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
+cv2.startWindowThread()
 # Function to perform face detection on an image
 def detect_faces(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
     return faces
 
 try:
@@ -43,3 +41,79 @@ except KeyboardInterrupt:
 finally:
     # Release resources
     camera.close()
+"""
+#this should do the same thing that chatgpt generated
+#my primary concern was with picam2.start this should start the video stream without recording which
+#was my main concern
+import cv2
+
+from picamera2 import Picamera2
+
+# Grab images as numpy arrays and leave everything else to OpenCV.
+
+face_detector = cv2.CascadeClassifier("/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml")
+cv2.startWindowThread()
+
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+picam2.start()
+
+while True:
+    im = picam2.capture_array()
+
+    grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    faces = face_detector.detectMultiScale(grey, 1.1, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0))
+
+    cv2.imshow("Camera", im)
+    cv2.waitKey(1)
+
+######################################################################
+######################################################################
+# below is a different verison that may have a performance boost
+######################################################################
+######################################################################
+#!/usr/bin/python3
+""" import time
+
+import cv2
+
+from picamera2 import MappedArray, Picamera2, Preview
+
+# This version creates a lores YUV stream, extracts the Y channel and runs the face
+# detector directly on that. We use the supplied OpenGL accelerated preview window
+# and delegate the face box drawing to its callback function, thereby running the
+# preview at the full rate with face updates as and when they are ready.
+
+face_detector = cv2.CascadeClassifier("/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml")
+
+
+def draw_faces(request):
+    with MappedArray(request, "main") as m:
+        for f in faces:
+            (x, y, w, h) = [c * n // d for c, n, d in zip(f, (w0, h0) * 2, (w1, h1) * 2)]
+            cv2.rectangle(m.array, (x, y), (x + w, y + h), (0, 255, 0, 0))
+
+
+picam2 = Picamera2()
+picam2.start_preview(Preview.QTGL)
+config = picam2.create_preview_configuration(main={"size": (640, 480)},
+                                             lores={"size": (320, 240), "format": "YUV420"})
+picam2.configure(config)
+
+(w0, h0) = picam2.stream_configuration("main")["size"]
+(w1, h1) = picam2.stream_configuration("lores")["size"]
+s1 = picam2.stream_configuration("lores")["stride"]
+faces = []
+picam2.post_callback = draw_faces
+
+picam2.start()
+
+start_time = time.monotonic()
+# Run for 10 seconds so that we can include this example in the test suite.
+while time.monotonic() - start_time < 10:
+    buffer = picam2.capture_buffer("lores")
+    grey = buffer[:s1 * h1].reshape((h1, s1))
+    faces = face_detector.detectMultiScale(grey, 1.1, 3) """
